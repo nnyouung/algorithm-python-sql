1a15e74b6a8b826ad117240c29c277d650f63bd2
nnyouung <hae22012@gmail.com>
2026-09-11 22:44:54 +0900

    [level 2] Title: 부모의 형질을 모두 가지는 대장균 찾기, Time: , Memory: undefined -BaekjoonHub

---
+++ MySQL/프로그래머스/2/301647. 부모의 형질을 모두 가지는 대장균 찾기/부모의 형질을 모두 가지는 대장균 찾기.sql
@@ -0,0 +1,5 @@
+SELECT child.ID, child.GENOTYPE, parent.GENOTYPE AS PARENT_GENOTYPE
+FROM ECOLI_DATA child
+JOIN ECOLI_DATA parent ON child.PARENT_ID = parent.ID
+WHERE parent.GENOTYPE & child.GENOTYPE = parent.GENOTYPE
+ORDER BY child.ID
